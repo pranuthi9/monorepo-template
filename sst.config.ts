4550b8fa6d86cae56e65ca804af5e81b51e530bb
Pranuthi Mangu <pranuthi.mangu@gmail.com>
2024-11-03 01:56:01 -0500

    Initial commit from template

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "time-capsule",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
